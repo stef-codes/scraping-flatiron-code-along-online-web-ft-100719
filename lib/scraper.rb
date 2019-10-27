126b77106ea88cff8eb96ef890944f80d0105a1b
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-27 06:48:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ class Scraper
   end
   
   def get_page
-    html = open(http://learn-co-curriculum.github.io/site-for-scraping/courses)
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
   
